d6c6e9d95771a3ffa061ea17de4406ebe1e98888
Johão Pedro <johaopedro99@gmail.com>
2021-10-29 16:04:30 -0300

    frases e textos foram adicionados

--- script.js
+++ script.js
@@ -24,29 +24,40 @@ botaoSubmit.addEventListener("click", function(e){
     tituloQuiz.classList.add("sumir")
     containerEscolhas.classList.add("sumir")
     containerResultado.classList.remove("sumir")
-    console.log(sum)
     if(sum===210){
-        tituloTipo.innerHTML="tipo Dragão"
         imagem.src="./img/dragon.jpg"
-        fraseTipo.innerHTML="Você realmente não tem medo de nada hein, parte pra cima com tudo sempre que pode e se garante na própria força."
+        tituloTipo.innerHTML="tipo Dragão"
+        fraseTipo.innerHTML="Você é uma pessoa forte que realmente não tem medo de nada hein, parte pra cima com tudo sempre que pode e se garante na própria força. Além de não se importar com os perigos que encontra pelo caminho"
     }
     if(sum===140){
         imagem.src="./img/ice.jpg"
+        tituloTipo.innerHTML="tipo Gelo"
+        fraseTipo.innerHTML="Ora ora, parece que temos alguém frio e calculista aqui, você pensa muito no seu próximo passo e não costuma agir por impulso e quando enfrenta situações difíceis, fica frio, apenas esperando a hora certa de fazer algo"
     }
     if(sum===70){
         imagem.src="./img/grass.jpg"
+        tituloTipo.innerHTML="tipo Grama"
+        fraseTipo.innerHTML="Você é uma pessoa bem de boa, que procura evitar problemas e que se apega sentimentalmente a tudo que você considera ter valor. Se desfazer de algo ou se separar de alguém é sempre um desafio pra você"
     }
     if(sum===0){
         imagem.src="./img/dark.jpg"
+        tituloTipo.innerHTML="tipo Noturno"
+        fraseTipo.innerHTML="Parece que temos alguém da zueira por aqui, você sempre tenta deixar o clima descontraído seja pra evitar o nervosismo ou simplesmente porque prefere risos ao invés de rostos sérios. Algumas pessoas tem dificuldade de diferenciar quando você está falando sério ou brincando"
     }
     if(sum<210 && sum>140){
         imagem.src="./img/fire.jpg"
+        tituloTipo.innerHTML="tipo Fogo"
+        fraseTipo.innerHTML="Olha, eu não iria querer ser seu inimigo, você é uma pessoa calorosa que se deixa levar pelo momento mas que também faz de tudo por aqueles que você se importa"
     }
     if(sum<140 && sum>70){
         imagem.src="./img/water.jpg"
+        tituloTipo.innerHTML="tipo Água"
+        fraseTipo.innerHTML="Se tem uma palavra que te define seria 'Sabedoria'. Você sabe lhe dar com os seus problemas mesmo achando que não faz ideia do que está fazendo, a prova disso é que raramente se mete em encrencas. Além de que, também sabe usar o ambiente para se proteger do problemas da vida"
     }
     if(sum<70 && sum>0){
         imagem.src="./img/normal.jpg"
+        tituloTipo.innerHTML="tipo Normal"
+        fraseTipo.innerHTML="Você literalmente pode se misturar com qualquer grupo de pessoas, é bem maleável e se adapta ao ambiente e ao assunto. Alguns podem até confundir isso com não ter personalidade própria mas a verdade é que você gosta de ver todos os lados e extrair o melhor deles"
     }
     return sum
 })
@@ -58,6 +69,7 @@ botaoReset.addEventListener("click", function(){
     tituloQuiz.classList.remove("sumir")
     containerResultado.classList.add("sumir")
     containerEscolhas.classList.remove("sumir")
+    location.reload()
 })
 
 
